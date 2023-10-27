37f597c1e84761b64d19df9b3d3bfe81121d26a9
Nithin <snithin816@gmail.com>
2023-10-27 11:07:15 +0530

    first commit

---
+++ src/components/StudentList.js
@@ -0,0 +1,41 @@
+import Axios from "axios";
+import { useEffect, useState } from "react";
+import StudentListRow from "./StudentListRow";
+
+function StudentList()
+{
+    const [arr,setArr] = useState([]);
+    useEffect(()=>{
+        Axios.get("https://ceud-dp1-bck.onrender.com/studentRoute")
+        .then((res)=>{
+            if(res.status === 200)
+                setArr(res.data);
+            else
+                Promise.reject();
+        })
+        .catch((err)=> alert(err));
+    },[]);
+
+    const ListItems = () =>{
+        return arr.map((val,ind)=>{  //[{_id,name,email,rollNo},{},{},{}]
+            return <StudentListRow obj={val}/>
+        })
+    }
+    return (
+        <table class="table table-bordered table-striped table-success">
+            <thead>
+                <tr>
+                    <th class="text-center">Name</th>
+                    <th class="text-center">Email</th>
+                    <th class="text-center">Roll Number</th>
+                    <th class="text-center">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {ListItems()}
+            </tbody>
+        </table>
+    )
+}
+export default StudentList;
+
